dca05f2460efe36e3e3e27fdc3b13371c90689c0
Punith Kashi <punithrkashi@gmail.com>
2017-03-20 21:53:31 -0400

    Revert "Test2"
    This reverts commit abe993097df4ba79897b2b6547fc0ed89846c357.

--- AndroidGeocodeLocation/app/src/main/java/android/javapapers/com/androidgeocodelocation/DatabaseHandler.java
+++ AndroidGeocodeLocation/app/src/main/java/android/javapapers/com/androidgeocodelocation/DatabaseHandler.java
@@ -8,7 +8,7 @@ import android.content.ContentValues;
 
         import java.util.ArrayList;
         import java.util.List;
-//test //test again
+//test
         import android.content.ContentValues;
         import android.content.Context;
         import android.database.Cursor;
